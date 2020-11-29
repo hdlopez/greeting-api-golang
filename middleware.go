41f63a79b66736742410684b026ab08d6ab4bfad
Horacio López <hdlopez@gmail.com>
2020-11-29 19:21:53 -0300

    Base project for part 0

---
+++ middleware.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func addCORS(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method")
+	c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+	c.Header("Allow", "GET, POST, OPTIONS, PUT, DELETE")
+
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
+
+	c.Next()
+}
